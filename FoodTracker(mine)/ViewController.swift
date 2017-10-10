5e924d6ec12fc4943858c4538c807ca20e898a05
dstrauss <dstrauss@novacoast.com>
2017-10-10 15:17:28 -0700

    Initial Commit

---
+++ FoodTracker(mine)/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FoodTracker(mine)
+//
+//  Created by Dominique Strauss on 10/10/17.
+//  Copyright © 2017 Dominique Strauss. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ FoodTracker(mine)Tests/FoodTracker_mine_Tests.swift
@@ -0,0 +1,36 @@
+//
+//  FoodTracker_mine_Tests.swift
+//  FoodTracker(mine)Tests
+//
+//  Created by Dominique Strauss on 10/10/17.
+//  Copyright © 2017 Dominique Strauss. All rights reserved.
+//
+
+import XCTest
+@testable import FoodTracker_mine_
+
+class FoodTracker_mine_Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
